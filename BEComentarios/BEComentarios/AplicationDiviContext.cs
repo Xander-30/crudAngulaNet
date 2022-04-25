3695f1e27bcb490c6e3b9c7d4e4fc0828dccc49e
Alex <hdelvalle29@gmail.com>
2022-04-24 20:31:56 -0500

    Se agrega el proyecto

---
+++ BEComentarios/BEComentarios/AplicationDiviContext.cs
@@ -0,0 +1,22 @@
+﻿using BEComentarios.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BEComentarios
+{
+    public class AplicationDiviContext : DbContext
+    {
+
+        public DbSet<Comentario> Comentario { get; set; }
+
+        public AplicationDiviContext(DbContextOptions<AplicationDiviContext>options):base(options)
+            
+         {
+         
+        }
+
+    }
+}
